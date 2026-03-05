a5da6416d86338b0cc6e0eb3441bef778ee4287a
juankhernadez83 <101617779+juankhernadez83@users.noreply.github.com>
2026-03-04 19:16:05 -0600

    1
    1

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+// cypress.config.js  — Req #9
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:4200',
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    supportFile: 'cypress/support/e2e.js',
+    video: false,
+    screenshotOnRunFailure: true,
+    viewportWidth: 1280,
+    viewportHeight: 720,
+  }
+});
